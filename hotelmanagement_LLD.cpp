7bbf6c623c9d32db6c984dbb1a7600028ee3fa0d
Piyush Aggarwal <piyush5807@gmail.com>
2021-06-20 14:36:00 +0530

    Update hotelmanagement_LLD.cpp

--- hotelmanagement_LLD.cpp
+++ hotelmanagement_LLD.cpp
@@ -8,27 +8,30 @@ public:
     string place;
     int pinecode;
 };
-class HotelData {
+class HotelData { // no point of creating hotelData class, it's a redundant class, all the atrributes in this class should be a part of hotel class itself
 public:
     string name;
     Address address;
     int rating;
-    int cost;
+    int cost;  // cost should not be associated with hotel, it should be associated with room
+    
+    // List of rooms should also be present as an attribute where room should be a separate class with contains room no, bed size, cost etc.
 };
+
 class userdata {
 public:
     string name;
     string email;
     Address address;
 };
-map<int, HotelData> HotelList;
-map<int, set<int>> hotelList;
+map<int, HotelData> HotelList; // parameter naming is highly incorrect, you should use something like hotelMap since it's not a list 
+map<int, set<int>> hotelList;  // chose different name, having same is not correct
 map<string, userdata> userList;
 class Hotel {
 private:
     HotelData hotel;
 public:
-    int key = rand();
+    int key = rand();             // unqiue key for every hotel should be present as an attribute in hotel class also, not only in map
     Hotel(string name, Address add, int rating, int cost) {
         hotel = {name, add, rating, cost};
         HotelList[key] = hotel;
@@ -45,6 +48,8 @@ public:
         v.erase(it);
         hotelList[hot.address.pinecode] = v;
     }
+    
+    // If the hotel's address is changed you are creating a new entry in the map but where are you deleting the old entry with old address ?
     void UpdateHotelData(int z, string name, Address add, int rating, int cost)
     {
         deletHotel();
@@ -55,8 +60,8 @@ public:
 };
 class User {
 private:
-    vector<HotelData> History;
-    userdata use;
+    vector<HotelData> History; // no need to store the entire hotel data in history, just store the hotel id and you can fetch the hotel details from some other map
+    userdata use; // again userdata is redundant class and also make your naming correct, what does use signifies ?
 public:
 
     User(string name, string email, Address address) {
@@ -73,6 +78,8 @@ public:
         auto it = userList.find(email);
         userList.erase(it);
         userList[email] = use;
+        
+        // These operations are wrong, you are updating user's email then rmeoving it from userlist why ? it ws not saved yet, you need to remove the prev email not the new one
     }
     void deletUser(string email)
     {
